c2bf945e6f4821f4c0c1e813d0c00fcbd2a26481
JoeyChien <39152035+JoeyChien@users.noreply.github.com>
2021-01-23 23:57:26 +0800

    建立文章及類別的 Model，並執行 Migrate

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length = 20)
+    create_time = models.DateTimeField(auto_now_add=True)
+
+class Post(models.Model):
+    title = models.CharField(max_length = 20)
+    content = models.TextField()
+    category = models.ForeignKey(Category, on_delete = models.CASCADE, related_name='post')
+    create_time = models.DateTimeField(auto_now_add=True)
