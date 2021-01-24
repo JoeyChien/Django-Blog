23d19bea0c5ad3458be9c95659764bddaa26e146
JoeyChien <39152035+JoeyChien@users.noreply.github.com>
2021-01-25 00:38:43 +0800

    修改文章預設顯示為標題及類別預設顯示名稱

--- posts/models.py
+++ posts/models.py
@@ -5,11 +5,17 @@ class Category(models.Model):
     name = models.CharField(max_length = 20)
     create_time = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
     title = models.CharField(max_length = 20)
     content = models.TextField()
     category = models.ForeignKey(Category, on_delete = models.CASCADE, related_name='post')
     create_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
