7b3c5fc39a1bbb9d964449a76e1f1a253e415be7
JoeyChien <39152035+JoeyChien@users.noreply.github.com>
2021-01-25 00:44:44 +0800

    設定在 {domian}/post 路徑下顯示所有文章的標題

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from posts.models import Post
 
 # Create your views here.
+def index(request):
+    posts = Post.objects.all()
+    output = '</br>'.join([p.title for p in posts])
+    return HttpResponse(output)
