0370d5081ed50ef85f4d6d5a92d46f0c4b3e8488
JoeyChien <39152035+JoeyChien@users.noreply.github.com>
2021-02-15 20:30:10 +0800

    在posts的view中修改index這個方法，使用index.html這個template去渲染出所有的文章

--- posts/views.py
+++ posts/views.py
@@ -5,5 +5,6 @@ from posts.models import Post
 # Create your views here.
 def index(request):
     posts = Post.objects.all()
-    output = '</br>'.join([p.title for p in posts])
-    return HttpResponse(output)
+    content = {'posts': posts}
+    return render(request, 'index.html', content)
+    
