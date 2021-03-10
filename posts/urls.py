91fe89ded833e17e6b9ad588071017694f5a760e
JoeyChien <39152035+JoeyChien@users.noreply.github.com>
2021-03-11 00:36:12 +0800

    新增 post 的 form，並且使用 form 建立新增 post 的表單頁面

--- posts/urls.py
+++ posts/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('create', views.post_create_view),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from posts.models import Post
+from .forms import PostForm
 
 # Create your views here.
 def index(request):
     posts = Post.objects.all()
     content = {'posts': posts}
     return render(request, 'index.html', content)
-    
+
+def post_create_view(request):
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+        form.save() 
+    context = {
+        'form' : form
+    } 
+    return   render(request, "posts/post_create.html", context)
