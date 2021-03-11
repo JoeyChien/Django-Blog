49611ba8c5fdbef59acffccb636255275d8ada92
JoeyChien <39152035+JoeyChien@users.noreply.github.com>
2021-03-11 14:47:19 +0800

    將 Post 的 Form 的欄位 label 改以中文顯示

--- posts/forms.py
+++ posts/forms.py
@@ -1,8 +1,13 @@
 from django import forms
-
+from django.utils.translation import gettext_lazy as _ 
 from .models import Post, Category
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = '__all__'
+        labels = {
+            'title'   : _('標題'),
+            'content' : _('內容'),
+            'category': _('分類'),
+        }
